d29871a1a23286f1dd48494c6afa8ba42068cc3b
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-07-11 13:46:36 +0300

    Make the app fill the screen.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 function App() {
     return (
-        <div>
+        <div className="App">
             <Header />
             <HomePage />
             <Footer />
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,44 +4,46 @@ import ihanaaElamaaLogo from "../images/ihanaaelamaa_logo.jpg";
 import Link from "./Link";
 const HomePage = () => {
     return (
-        <div className="HomePage">
-            <h1>Title Goes Here</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                est laborum.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                est laborum.
-            </p>
-            <div className="LinkContainer">
-                <Link
-                    imageData={{ src: instagramLogo, alt: "Instagram Logo" }}
-                    text="@instagram_handle"
-                    link="https://www.instagram.com"
-                />
-                <Link
-                    imageData={{ src: emailIcon, alt: "Email icon" }}
-                    text="abc@example.com"
-                    link="mailto: abc@example.com"
-                />
-                <Link
-                    imageData={{ src: ihanaaElamaaLogo, alt: "Ihanaa Elämää Logo" }}
-                    text="ihanaaelamaa.fi"
-                    link="https://www.ihanaaelamaa.fi"
-                />
+        <main className="HomePage">
+            <div className="Inner">
+                <h1>Title Goes Here</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div className="LinkContainer">
+                    <Link
+                        imageData={{ src: instagramLogo, alt: "Instagram Logo" }}
+                        text="@instagram_handle"
+                        link="https://www.instagram.com"
+                    />
+                    <Link
+                        imageData={{ src: emailIcon, alt: "Email icon" }}
+                        text="abc@example.com"
+                        link="mailto: abc@example.com"
+                    />
+                    <Link
+                        imageData={{ src: ihanaaElamaaLogo, alt: "Ihanaa Elämää Logo" }}
+                        text="ihanaaelamaa.fi"
+                        link="https://www.ihanaaelamaa.fi"
+                    />
+                </div>
             </div>
-        </div>
+        </main>
     );
 };
 
