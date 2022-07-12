daeeb6c7ad5f0db4168804ed7c48612a934f29f4
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-07-12 20:54:15 +0300

    Change site language to en, translate footer accordingly.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ const Footer = () => {
     return (
         <footer>
             <div className="Inner">
-                <div>Kuva: @Biitujumi PhotoGraphy</div>
-                <div>Sivu: Atte Ala-Lahti</div>
+                <div>Photo: @Biitujumi PhotoGraphy</div>
+                <div>Web Page: Atte Ala-Lahti</div>
             </div>
         </footer>
     );
