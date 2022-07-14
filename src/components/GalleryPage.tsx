0a3a5287b2866da5b0b9272cd086611a57cddb5b
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-07-14 23:38:55 +0300

    Modify GalleryPage: translate all text.

--- src/components/GalleryPage.tsx
+++ src/components/GalleryPage.tsx
@@ -5,11 +5,13 @@ import image3 from "../images/gallery3.jpg";
 import image4 from "../images/gallery4.jpg";
 import image5 from "../images/gallery5.jpg";
 import image6 from "../images/gallery6.jpg";
+import { useTranslation } from "react-i18next";
 
 const GalleryPage = () => {
+    const { t } = useTranslation("gallery");
     return (
         <main className="GalleryPage">
-            <h1>Galleria</h1>
+            <h1>{t("gallery")}</h1>
             <div className="Inner">
                 <GalleryEntry
                     image={image1}
