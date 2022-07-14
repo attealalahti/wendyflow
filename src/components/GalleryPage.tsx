efe2d344c1088f7dea31213e2a683d1f8e21fe78
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-07-14 23:36:11 +0300

    Modify GalleryPage: add more images.

--- src/components/GalleryPage.tsx
+++ src/components/GalleryPage.tsx
@@ -1,12 +1,39 @@
-import image from "../images/background_image.jpg";
 import GalleryEntry from "./GalleryEntry";
+import image1 from "../images/gallery1.jpg";
+import image2 from "../images/gallery2.jpg";
+import image3 from "../images/gallery3.jpg";
+import image4 from "../images/gallery4.jpg";
+import image5 from "../images/gallery5.jpg";
+import image6 from "../images/gallery6.jpg";
+
 const GalleryPage = () => {
     return (
         <main className="GalleryPage">
             <h1>Galleria</h1>
             <div className="Inner">
                 <GalleryEntry
-                    image={image}
+                    image={image1}
+                    alt="Wendy Flow"
+                    credit="Flashing Porridge"
+                />
+                <GalleryEntry image={image2} alt="Wendy Flow" credit="Gloria Grafia" />
+                <GalleryEntry
+                    image={image3}
+                    alt="Wendy Flow"
+                    credit="Flashing Porridge"
+                />
+                <GalleryEntry
+                    image={image4}
+                    alt="Wendy Flow"
+                    credit="Flashing Porridge"
+                />
+                <GalleryEntry
+                    image={image5}
+                    alt="Wendy Flow"
+                    credit="Biitujumi PhotoGraphy"
+                />
+                <GalleryEntry
+                    image={image6}
                     alt="Wendy Flow"
                     credit="Biitujumi PhotoGraphy"
                 />
