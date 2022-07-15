0141b30b6a35c74f85243280d72e49f6839915ba
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-07-15 12:37:17 +0300

    Modify index.html and App: add translated page title.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,14 @@ import HomePage from "./HomePage";
 import Footer from "./Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GalleryPage from "./GalleryPage";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 const App = () => {
+    const { t, i18n } = useTranslation();
+    useEffect(() => {
+        document.title = t("meta.title");
+    }, [t, i18n.language]);
     return (
         <BrowserRouter>
             <div className="App">
